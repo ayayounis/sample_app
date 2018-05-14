55441a3555311e91e5911342d58a39341922f066
ayayounis <ayayounis@gmail.com>
2018-05-14 11:06:45 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'static_pages#home'
   get '/help',to: 'static_pages#help', as:'help'
   get 'static_pages/about' => 'static_pages#about'
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  resources :users
 end
