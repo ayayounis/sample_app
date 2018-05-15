4f1524b888613cca0d8267f91e28ff0038ffaaec
ayayounis <ayayounis@gmail.com>
2018-05-15 16:09:41 +0300

    updste

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/help',to: 'static_pages#help', as:'help'
   get 'static_pages/about' => 'static_pages#about'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end
